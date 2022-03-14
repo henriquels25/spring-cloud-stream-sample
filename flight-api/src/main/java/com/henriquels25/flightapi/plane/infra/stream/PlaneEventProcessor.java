7b1516c4a740078294fcd8468c4ac79c84e5bf30
Henrique Schmidt <henriquels25@gmail.com>
2022-03-14 17:20:07 -0300

    altera processor para acessar os headers

--- flight-api/src/main/java/com/henriquels25/flightapi/plane/infra/stream/PlaneEventProcessor.java
+++ flight-api/src/main/java/com/henriquels25/flightapi/plane/infra/stream/PlaneEventProcessor.java
@@ -3,21 +3,24 @@ package com.henriquels25.flightapi.plane.infra.stream;
 import com.henriquels25.flightapi.flight.Flight;
 import com.henriquels25.flightapi.flight.FlightOperations;
 import lombok.AllArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
 @Component("planeEventProcessor")
 @AllArgsConstructor
-class PlaneEventProcessor implements Function<PlaneEvent, FlightEvent> {
+class PlaneEventProcessor implements Function<Message<PlaneEvent>, Message<FlightEvent>> {
 
     private final FlightOperations flightOperations;
 
     @Override
-    public FlightEvent apply(PlaneEvent planeEvent) {
-        String planeId = planeEvent.getPlaneId();
+    public Message<FlightEvent> apply(Message<PlaneEvent> planeEvent) {
+        String planeId = planeEvent.getPayload().getPlaneId();
         Flight flight = flightOperations.findConfirmedFlightByPlaneId(planeId)
                 .orElseThrow(() -> new NoFlightFoundException(String.format("No flight found for plane id %s", planeId)));
-        return new FlightEvent(flight.getId(), planeEvent.getCurrentAirport());
+        return new GenericMessage<>(new FlightEvent(flight.getId(),
+                planeEvent.getPayload().getCurrentAirport()));
     }
 }
