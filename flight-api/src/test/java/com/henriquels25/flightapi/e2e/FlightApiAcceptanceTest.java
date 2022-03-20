b8f5e50168daddcb15e9b11aae18837369da08eb
Henrique Schmidt <henriquels25@gmail.com>
2022-03-20 16:57:13 -0300

    Fix code smell in E2E test

--- flight-api/src/test/java/com/henriquels25/flightapi/e2e/FlightApiAcceptanceTest.java
+++ flight-api/src/test/java/com/henriquels25/flightapi/e2e/FlightApiAcceptanceTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @EmbeddedKafkaWithTopics
 @AutoConfigureMockMvc
-public class FlightApiAcceptanceTest {
+class FlightApiAcceptanceTest {
 
     @Autowired
     private EmbeddedKafkaBroker broker;
